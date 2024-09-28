13edc9574c52034ec2b02ad20ad7adbf08fb0632
GSPVK <theunderlike@gmail.com>
2024-09-28 18:05:28 +0300

    feat(scraper): refactor scraping logic into modular functions with error handling

--- anirecombot/scraper.py
+++ anirecombot/scraper.py
@@ -1,53 +1,76 @@
-from bs4 import BeautifulSoup
+from logging import getLogger
+from typing import List
+
+from bs4 import BeautifulSoup, Tag
 from selenium import webdriver
-
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common import TimeoutException
 from selenium.webdriver import FirefoxOptions
+from selenium.webdriver.common.by import By
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+
+logger = getLogger(__name__)
 
 
-def scrape(mal_nickname: str) -> tuple:
+def parse_html(html_text: str):
+    soup = BeautifulSoup(html_text, 'html.parser')
+    recs = soup.find_all('div', class_='recommendation svelte-k28mqj')
+    return recs
+
+
+def get_links(recs: List[Tag], driver: WebDriver, timeout: int = 5):
+    links = []
+    for i in range(1, len(recs) + 1):
+        try:
+            button = WebDriverWait(driver, timeout).until(
+                EC.element_to_be_clickable((By.XPATH, f'/html/body/div[1]/div[3]/div[2]/div[{i}]/div/div[2]'))
+            )
+            button.click()
+            link = WebDriverWait(driver, timeout).until(
+                EC.presence_of_element_located((By.XPATH, f'/html/body/div[1]/div[3]/div[2]/div[{i}]/div/div[1]/div/a'))
+            )
+            links.append(link.get_attribute('href'))
+        except TimeoutException:
+            logger.error(f'Failed to find or click the button/link for recommendation {i} on {driver.current_url}')
+        except Exception as e:
+            logger.error(f'Unexpected error for recommendation {i}: {e}')
+    return links
+
+
+def scrape(mal_nickname: str, recs_num: int = 50) -> tuple:
     """
     Scrape the recommendations list from https://anime.ameo.dev/
 
 
     :param mal_nickname: MAL username
+    :param recs_num: Number of recommendations to scrape
     :return: List of anime links, recommendation page
     """
     firefox_options = FirefoxOptions()
-    firefox_options.add_argument("--headless")
-    # For a clear demonstration of scraping/debugging, remove "options" from the "driver" variable
-    driver = webdriver.Firefox(options=firefox_options)
+    firefox_options.add_argument('--headless')
 
-    url = f'https://anime.ameo.dev/user/{mal_nickname}/recommendations'
-    driver.get(url)
+    with webdriver.Firefox(options=firefox_options) as driver:
+        url = f'https://anime.ameo.dev/user/{mal_nickname}/recommendations'
+        driver.get(url)
+        try:
+            WebDriverWait(driver, 10).until(
+                lambda browser: len(driver.find_elements(By.CSS_SELECTOR, '.recommendation.svelte-k28mqj')) == recs_num
+            )
+        except TimeoutException:
+            logger.error(
+                f'Only {len(driver.find_elements(By.CSS_SELECTOR, ".recommendation.svelte-k28mqj"))} recommendations found.'
+            )
+        except Exception as e:
+            logger.error(f'Unexpected error: {e}')
 
-    recs_num = 50  # number of recommendations to scrape (0...50)
-    WebDriverWait(driver, 10).until(
-        lambda browser: len(driver.find_elements(By.CSS_SELECTOR, '.recommendation.svelte-k28mqj')) == recs_num)
-    html_text = driver.page_source
-    soup = BeautifulSoup(html_text, 'html.parser')
-    recs = soup.find_all('div', class_='recommendation svelte-k28mqj')
+        recs = parse_html(html_text=driver.page_source)
+        links = get_links(recs=recs, driver=driver)
 
-    links = []
-    for i in range(1, len(recs) + 1):
-        WebDriverWait(driver, 5).until(
-            EC.element_to_be_clickable((By.XPATH, f'/html/body/div[1]/div[3]/div[2]/div[{i}]/div/div[2]'))
-        )
-        driver.find_element(By.XPATH, f'/html/body/div[1]/div[3]/div[2]/div[{i}]/div/div[2]').click()
-        WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.XPATH, f'/html/body/div[1]/div[3]/div[2]/div[{i}]/div/div[1]/div/a'))
-        )
-        link = driver.find_element(By.XPATH, f'/html/body/div[1]/div[3]/div[2]/div[{i}]/div/div[1]/div/a')
-        link = link.get_attribute('href')
-        links.append(link)
-
-    driver.quit()
     return links, recs
 
 
-def get_recs(links: list, recs: list) -> list:
+def create_cards(links: list, recs: list) -> list:
     """
     Create a dictionary with information about recommendations.
 
@@ -79,5 +102,5 @@ def create_recommendations(mal_nickname: str) -> list:
     :param mal_nickname: MAL username
     """
     links, recs = scrape(mal_nickname)
-    recomms = get_recs(links, recs)
+    recomms = create_cards(links, recs)
     return recomms
